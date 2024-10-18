23fb3226b698be9296c032cee6b80394bd81cbf9
joaozinhooh <joaoviniciusneimannramos8@gmail.com>
2024-10-17 22:22:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'ingles',
+    'como se pronuncia azul em ingles?',
+    'Se pronuncia blue'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'A do brasil é brasilia'
+)
+
+criaCartao(
+    'matematica',
+    'qual a raiz quadrada de 9?',
+    'A raiz quadrada de 9 é 3'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'matematica',
+    ' Qual numero vem depois do 9?',
+    'é o 10'
+)
+
+criaCartao(
+    'adivinha',
+    'Oque é Oque é tem bocas mais nao fala?',
+    'O fogão'
+)
